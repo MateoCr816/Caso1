a0e07292490951bc9bad562d1650009aea872e13
Gabriel Padilla <g.padilla@uniandes.edu.co>
2024-09-05 11:35:09 -0500

    atributos productor

--- src/caso/Productor.java
+++ src/caso/Productor.java
@@ -1,5 +1,20 @@
 package caso;
 
 public class Productor {
-    
+    private String tipo;
+
+    // Constructor
+    public Productor(String tipo) {
+        this.tipo = tipo;
+    }
+
+    // Getter
+    public String getTipo() {
+        return tipo;
+    }
+
+    // Setter
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
